cc9ddda5f7a7f5ebe2b0a2a82e3b09e642cf7892
Rafael Dantas Justo <rafael@registro.br>
2015-09-11 11:02:42 -0300

    Update RDAP library

--- Godeps/_workspace/src/github.com/registrobr/rdap/protocol/domain.go
+++ Godeps/_workspace/src/github.com/registrobr/rdap/protocol/domain.go
@@ -2,28 +2,21 @@ package protocol
 
 // Domain describes Domain Object Class as it is in RFC 7483, section 5.3
 type Domain struct {
-	ObjectClassName string                `json:"objectClassName"`
-	Handle          string                `json:"handle,omitempty"`
-	LDHName         string                `json:"ldhName,omitempty"`
-	UnicodeName     string                `json:"unicodeName,omitempty"`
-	Nameservers     []Nameserver          `json:"nameservers,omitempty"`
-	SecureDNS       *SecureDNS            `json:"secureDNS,omitempty"`
-	Arbitration     bool                  `json:"nicbr_arbitration,omitempty"`
-	Links           []Link                `json:"links,omitempty"`
-	Entities        []Entity              `json:"entities,omitempty"`
-	Events          []Event               `json:"events,omitempty"`
-	Status          []Status              `json:"status,omitempty"`
-	PublicIDs       []PublicID            `json:"publicIds,omitempty"`
-	Remarks         []Remark              `json:"remarks,omitempty"`
-	Notices         []Notice              `json:"notices,omitempty"`
-	Network         *IPNetwork            `json:"network,omitempty"`
-	Unavailability  *DomainUnavailability `json:"-"`
+	ObjectClassName string       `json:"objectClassName"`
+	Handle          string       `json:"handle,omitempty"`
+	LDHName         string       `json:"ldhName,omitempty"`
+	UnicodeName     string       `json:"unicodeName,omitempty"`
+	Nameservers     []Nameserver `json:"nameservers,omitempty"`
+	SecureDNS       *SecureDNS   `json:"secureDNS,omitempty"`
+	Arbitration     bool         `json:"nicbr_arbitration,omitempty"`
+	Links           []Link       `json:"links,omitempty"`
+	Entities        []Entity     `json:"entities,omitempty"`
+	Events          []Event      `json:"events,omitempty"`
+	Status          []Status     `json:"status,omitempty"`
+	PublicIDs       []PublicID   `json:"publicIds,omitempty"`
+	Remarks         []Remark     `json:"remarks,omitempty"`
+	Notices         []Notice     `json:"notices,omitempty"`
+	Network         *IPNetwork   `json:"network,omitempty"`
+	Unavailability  string       `json:"-"`
 	Conformance
-}
-
-// DomainUnavailability is a NIC.br extension used to determinate the reason
-// that a domain name cannot be registered
-type DomainUnavailability struct {
-	Reason     string `json:"nicbr_reason"`
-	INPINumber int    `json:"nicbr_inpiNumber,omitempty"`
 }
