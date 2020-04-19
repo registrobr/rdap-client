78abd81cd917ddd0e813dacff457f41aff76d213
MendelGusmao <mendelsongusmao@gmail.com>
2020-04-19 14:15:32 -0300

    Removing Godeps and vendor directories

--- vendor/github.com/registrobr/rdap/doc.go
+++
@@ -1,4 +0,0 @@
-// Package rdap is a RDAP (Registration Data Access Protocol) library to be
-// used in clients and servers to make the life easier when building requests and
-// responses.
-package rdap
--- vendor/github.com/registrobr/rdap/protocol/doc.go
+++
@@ -1,3 +0,0 @@
-// Package protocol contains RDAP protocol types defined in RFC 7483 and in
-// NIC.br extension document.
-package protocol
